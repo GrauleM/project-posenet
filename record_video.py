aa4bef695ae4ba10faf2f0430a2e4847576742dd
graulem <moritz@graule.ch>
2021-05-24 15:22:57 -0400

    change fps

--- record_video.py
+++ record_video.py
@@ -1,12 +1,12 @@
 import picamera     # Importing the library for camera module
 from time import sleep  # Importing sleep from time library to add delay in program
-camera = picamera.PiCamera(resolution = (640, 480),framerate=90)    # Setting up the camera
+camera = picamera.PiCamera(resolution = (1280, 720),framerate=90)    # Setting up the camera
 camera.rotation = 180
 camera.start_preview()      # You will see a preview window while recording
 print("start recording in 5s")
 sleep(5)
 print("start recording")
-camera.start_recording('/home/pi/Desktop/video_640x480.h264') # Video will be saved at desktop
+camera.start_recording('/home/pi/Desktop/video_1280x720.h264') # Video will be saved at desktop
 for i in range(60):
     print(f"{i} seconds")
     sleep(1)
