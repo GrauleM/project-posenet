4f73d7e39fe17b877088d406e578f57207ccb107
graulem <moritz@graule.ch>
2021-05-24 12:56:59 -0400

    cam recording

--- record_video.py
+++ record_video.py
@@ -2,8 +2,12 @@ import picamera     # Importing the library for camera module
 from time import sleep  # Importing sleep from time library to add delay in program
 camera = picamera.PiCamera()    # Setting up the camera
 camera.start_preview()      # You will see a preview window while recording
+print("start recording in 5s")
 sleep(5)
-camera.stop_preview()
+print("start recording in 5s")
 camera.start_recording('/home/pi/Desktop/video.h264') # Video will be saved at desktop
-sleep(60)
+for i in range(60):
+    print(f"{i} seconds")
+    sleep(1)
 camera.stop_recording()
+camera.stop_preview()
